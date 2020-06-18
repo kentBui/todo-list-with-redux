dfaad4bbb41afa62550d396d823e63902a7326cc
kentFrontEndDev <duccuong.hpe@gmail.com>
2020-06-18 12:34:51 +0700

    add strict mode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
